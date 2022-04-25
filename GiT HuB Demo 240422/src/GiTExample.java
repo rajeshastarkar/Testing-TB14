cee0e174c6382a8eb795001d5562ee4e9e42f092
Admin# <Admin#@DESKTOP-P14M2E2>
2022-04-25 13:01:29 +0530

    BY rrraaajjjeeessshhh pulland push

--- GiT HuB Demo 240422/src/GiTExample.java
+++ GiT HuB Demo 240422/src/GiTExample.java
@@ -1,10 +1,21 @@
 
 
   public class GiTExample
-  { public static void main(String[] args) 
+  { 
+	  
+	  public void m1()
+	  {
+		  System.out.println("This  is my Method");
+	  }
+	  
+	  public static void main(String[] args) 
   
   {
     System.out.println("Hello Git Hub");
+    
+    GiTExample git=new GiTExample();
+    git.m1();
+    
   }
   
   }
