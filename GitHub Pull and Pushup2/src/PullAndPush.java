0c759ba3379a5f72050b142cf8bc46399f3d9903
Rajesh G.Astarkar <104189277+rajeshastarkar@users.noreply.github.com>
2022-04-25 13:35:38 +0530

    Update PullAndPush.java

--- GitHub Pull and Pushup2/src/PullAndPush.java
+++ GitHub Pull and Pushup2/src/PullAndPush.java
@@ -11,6 +11,11 @@ public class PullAndPush
 		System.out.println("This is m2 Method");
 	}
 	
+	public void m3()
+	{
+		System.out.println("This is m3 Method");
+	}
+	
 	public static void main(String[] args) 
 	{
 		System.out.println("Amit Sir");
